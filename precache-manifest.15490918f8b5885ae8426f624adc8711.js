04de7b141029539a7e5ae4e915d84c4fea548fc5
Pawel-Dlugosz <Pawel-Dlugosz@users.noreply.github.com>
2021-09-12 16:04:08 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.15490918f8b5885ae8426f624adc8711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a30d687f13628c857a3446a158b36f9c",
+    "url": "https://Pawel-Dlugosz.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "764f4684b491061b4002",
+    "url": "https://Pawel-Dlugosz.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "9558fa6212a58467ec19",
+    "url": "https://Pawel-Dlugosz.github.io/task-management-frontend/static/js/2.e63a6b33.chunk.js"
+  },
+  {
+    "revision": "764f4684b491061b4002",
+    "url": "https://Pawel-Dlugosz.github.io/task-management-frontend/static/js/main.eea740b6.chunk.js"
+  },
+  {
+    "revision": "e464f1205983beabd574",
+    "url": "https://Pawel-Dlugosz.github.io/task-management-frontend/static/js/runtime~main.70e5f07c.js"
+  }
+]);
